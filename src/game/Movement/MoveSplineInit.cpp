ccc0700bcb51cd6ccc1982618f45e89ac13e8a3e
Chron-cmangos <chrongh@proton.me>
2026-09-25 23:33:07 +0200

    Reverted "fix(movement): catch up monster move spline on move stop and unit death"
    Gave compilation error, need to look through more of the code

--- src/game/Movement/MoveSplineInit.cpp
+++ src/game/Movement/MoveSplineInit.cpp
@@ -66,10 +66,8 @@ namespace Movement
 
         // there is a big chance that current position is unknown if current state is not finalized, need compute it
         // this also allows calculate spline position and update map position in much greater intervals
-        if (!move_spline.Finalized() && !transportInfo)
+        if (!move_spline.Finalized())
             real_position = move_spline.ComputePosition();
-        else if (!transportInfo && !transport)
-            real_position.z -= unit.GetHoverOffset(); // ground-relative; hover is reapplied in UpdateSplinePosition
 
         bool pathEmpty = false;
         if (args.path.empty())
@@ -111,7 +109,6 @@ namespace Movement
 
         unit.m_movementInfo.SetMovementFlags(MovementFlags(moveFlags));
         move_spline.Initialize(args);
-        unit.SetLastSplineStepTime();
 
         WorldPacket data(SMSG_MONSTER_MOVE, 64);
         data << unit.GetPackGUID();
